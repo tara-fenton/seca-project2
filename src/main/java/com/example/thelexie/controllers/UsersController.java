ea2de90074ae44896f0eae4509417d98f06bf996
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-05-28 16:54:51 -0400

    add, edit and delete user api

--- src/main/java/com/example/thelexie/controllers/UsersController.java
+++ src/main/java/com/example/thelexie/controllers/UsersController.java
@@ -3,8 +3,10 @@ package com.example.thelexie.controllers;
 import com.example.thelexie.models.User;
 import com.example.thelexie.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UsersController {
@@ -12,8 +14,37 @@ public class UsersController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping("/api/users")
     public Iterable<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/api/users/{userId}")
+    public Optional<User> findUserById(@PathVariable Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    @DeleteMapping("/api/users/{userId}")
+    public HttpStatus deleteUserById(@PathVariable Long userId) {
+        userRepository.deleteById(userId);
+        return HttpStatus.OK;
+    }
+
+    @PostMapping("/api/users")
+    public User createNewUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @PatchMapping("/api/users/{userId}")
+    public User updateUserById(@PathVariable Long userId, @RequestBody User userRequest) {
+
+        User userFromDb = userRepository.findById(userId).get();
+
+        userFromDb.setFirstName(userRequest.getFirstName());
+        userFromDb.setLastName(userRequest.getLastName());
+        userFromDb.setEmail(userRequest.getEmail());
+        userFromDb.setGender(userRequest.getGender());
+
+        return userRepository.save(userFromDb);
     }
 }
