b6382dfb815665f8465098768de4fbd813619012
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-05-28 14:44:16 -0400

    user controller, /users displays data

---
+++ src/main/java/com/example/thelexie/controllers/UsersController.java
@@ -0,0 +1,19 @@
+package com.example.thelexie.controllers;
+
+import com.example.thelexie.models.User;
+import com.example.thelexie.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public Iterable<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+}
